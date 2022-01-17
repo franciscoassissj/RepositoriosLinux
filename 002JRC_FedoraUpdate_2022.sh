341a56787d91a9f74f88706b180113c87bbfecb2
Francisco Junior <francisco.assissj@gmail.com>
2022-01-17 17:54:23 -0300

    Adcionando arquivo 002JRC_FedoraUpdate_2022.sh

---
+++ 002JRC_FedoraUpdate_2022.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+# FedoraUpdate.sh
+
+# Script para atualização e instalação de ferramentas mp Linux Fedora 35
+# Autor: Francisco Junior
+# E-mail: francisco.assissj@gmail.com
+# Versão: 1.0
